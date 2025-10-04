472949e656403c8d69e3d5fa7df08d7b4c52eb9e
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2025-10-05 00:21:46 +0200

    1.21.1 Backport

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,11 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.21.2")
+    minecraft("com.mojang:minecraft:1.21.1")
     mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:0.16.14")
-    modImplementation("com.terraformersmc:modmenu:12.0.0")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.106.1+1.21.2")
+    modImplementation("com.terraformersmc:modmenu:11.0.1")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.116.6+1.21.1")
     implementation("com.google.code.gson:gson:2.13.1")
 }
 
@@ -32,20 +32,20 @@ tasks {
     }
 
     remapJar {
-        archiveBaseName = "ClientSpoofer-1.21.2"
+        archiveBaseName = "ClientSpoofer-1.21.1"
     }
 }
 
 modrinth {
     token = System.getenv("MODRINTH_TOKEN")
     projectId = "nWJHVhGM"
-    versionName = "$version (1.21.2)"
-    versionNumber = "$version-1.21.2"
+    versionName = "$version (1.21.1)"
+    versionNumber = "$version-1.21.1"
     versionType = if (version.toString().contains("alpha")) "alpha"
     else if (version.toString().contains("beta")) "beta"
     else "release"
     uploadFile = tasks.remapJar.get()
-    gameVersions = listOf("1.21.2")
+    gameVersions = listOf("1.21.1")
     loaders = listOf("fabric")
     dependencies {
         optional.project("modmenu")
